ae3ef04a769b0c670fe623a2d5c5fa788f4c5a31
Will Bamberg <wbamberg@mozilla.com>
2017-01-23 20:27:41 -0800

    Added CodeMirror support

--- editable-samples/js/editable.js
+++ editable-samples/js/editable.js
@@ -2,35 +2,77 @@ var element = document.getElementById("example-element");
 var input = document.getElementById("input");
 var reset = document.getElementById("reset");
 var edit = document.getElementById("edit");
-var code = input.value;
+
+var cmOptions = {
+  mode: "css",
+  theme: "mdn-like"
+}
+
+var cmEditor = CodeMirror(document.body, cmOptions);
+cmEditor.setSize("100%", 50);
+cmEditor.doc.setValue(cmInitContent);
+
+CodeMirror.hint.css = function(cm) {
+  var inner = {from: cm.getCursor(), to: cm.getCursor(), list: []};
+  
+  var currentPos = cm.getCursor();
+  var preceding = cm.getRange({line: currentPos.line, ch: 0}, currentPos);
+  if (preceding == cmMatchToShowCompletions) {
+    inner.list = cmCompletionChoices;
+  }
+  return inner;
+};
 
 function applyCode() {
-  element.style.cssText = input.value;
+  element.style.cssText = cmEditor.doc.getValue();
 }
 
 reset.addEventListener("click", function() {
-  input.value = code;
+  cmEditor.doc.setValue(cmInit);
   applyCode();
 });
 
-function setSelection() {
-  var start = input.value.indexOf(":") + 1;
-  if ((input.value.length > start) && (input.value[start] === " ")) {
+function selectValue() {
+  var value = cmEditor.doc.getValue();
+  var start = value.indexOf(":") + 1;
+  if ((value.length > start) && (value[start] === " ")) {
     start++;
   }
 
-  var end = input.value.length - 1;
-  if ((input.value.length > 0) && (input.value[end-1] === ";")) {
+  var end = value.length - 1;
+  if ((value.length > 0) && (value[end-1] === ";")) {
     end--;
   }
 
-  input.setSelectionRange(start, end);
+  cmEditor.doc.setSelection(
+    {line: 0, ch: start},
+    {line: 0, ch: end});
 }
 
 edit.addEventListener("click", function() {
-  input.focus();
-  setSelection();
+  cmEditor.focus();
+  selectValue();
 });
 
-input.addEventListener("input", applyCode);
+cmEditor.on("change", applyCode);
 window.addEventListener("load", applyCode);
+
+function showCompletions(cm, event) {
+  var popupKeyCodes = {
+      "9": "tab",
+      "13": "enter",
+      "27": "escape",
+      "33": "pageup",
+      "34": "pagedown",
+      "35": "end",
+      "36": "home",
+      "38": "up",
+      "40": "down"
+  }
+
+if(!popupKeyCodes[(event.keyCode || event.which).toString()]) {
+    CodeMirror.showHint(cm, CodeMirror.hint.css, {completeSingle: false, closeOnUnfocus:false});
+  }           
+}
+
+cmEditor.on("keyup", showCompletions);
---
+++ editable-samples/pages/border-top-color/js/border-top-color.js
@@ -0,0 +1,12 @@
+var cmInitContent = 'border-top-color: red;';
+
+var cmMatchToShowCompletions = "border-top-color: ";
+
+var cmCompletionChoices = [
+  'red',
+  'rgb(255, 128, 0)',
+  'hsla(240, 50%, 25%, 0.75)',
+  '#ffbb00',
+  'currentColor',
+  'transparent'
+];
