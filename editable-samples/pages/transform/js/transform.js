7828bfbc75333841949ea283a257c901c583cc0c
Will Bamberg <wbamberg@mozilla.com>
2017-03-01 14:10:55 -0800

    Added interactive example for

---
+++ editable-samples/pages/transform/js/transform.js
@@ -0,0 +1,12 @@
+var cmInitContent = 'transform: skew(30deg, 20deg);';
+
+var cmMatchToShowCompletions = "transform: ";
+
+var cmCompletionChoices = [
+  'scale(2, 0.5)',
+  'rotate(0.5turn)',
+  'skew(30deg, 20deg)',
+  'translateX(12em)',
+  'matrix(1.0, 2.0, 3.0, 4.0, 5.0, 6.0)',
+  'translateX(100px) rotate(180deg) skew(30deg, 20deg)'
+];
