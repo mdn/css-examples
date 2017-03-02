368fdcca9a46c4cbf5777461259bb19ee15ef5b6
Will Bamberg <wbamberg@mozilla.com>
2017-03-01 16:26:26 -0800

    Add caption style

--- editable-samples/pages/font/js/font.js
+++ editable-samples/pages/font/js/font.js
@@ -5,5 +5,6 @@ var cmMatchToShowCompletions = "font: ";
 var cmCompletionChoices = [
   '1.5em/1.2 courier',
   'italic 2em cursive',
-  'italic small-caps bolder condensed 24px/2 cursive'
+  'italic small-caps bolder condensed 24px/2 cursive',
+  'caption'
 ];
