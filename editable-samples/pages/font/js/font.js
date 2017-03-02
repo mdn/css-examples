4ab06a1d0b1726909df99896b26a2ba5cabbbc92
Will Bamberg <wbamberg@mozilla.com>
2017-03-02 12:36:53 -0800

    Change autocomplete options

--- editable-samples/pages/font/js/font.js
+++ editable-samples/pages/font/js/font.js
@@ -3,8 +3,8 @@ var cmInitContent = 'font: italic 2em cursive;';
 var cmMatchToShowCompletions = "font: ";
 
 var cmCompletionChoices = [
-  '1.5em/1.2 courier',
   'italic 2em cursive',
-  'italic small-caps bolder condensed 24px/2 cursive',
+  '1.5em/1.2 courier',
+  'small-caps bold 24px/2 sans-serif',
   'caption'
 ];
