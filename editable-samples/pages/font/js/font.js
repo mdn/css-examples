8c71c46d59bc6fc9ba621849e72226f39731938a
Will Bamberg <wbamberg@mozilla.com>
2017-03-01 16:14:36 -0800

    Added example for font

---
+++ editable-samples/pages/font/js/font.js
@@ -0,0 +1,9 @@
+var cmInitContent = 'font: 1em/1.5 "Open Sans", sans-serif;';
+
+var cmMatchToShowCompletions = "font: ";
+
+var cmCompletionChoices = [
+  '1.5em/1.2 courier',
+  'italic 2em cursive',
+  'italic small-caps bolder condensed 24px/2 cursive'
+];
