d89f09b6175738a21b9fbeccf307bfb6412d2772
chrisdavidmills <chrisdavidmills@gmail.com>
2018-01-23 16:05:13 +0000

    overscroll-behavior property demo

---
+++ overscroll-behavior/main.js
@@ -0,0 +1,33 @@
+// Generate fake contacts
+
+let mainElem = document.querySelector('main');
+
+for(var i = 1; i < 51; i++) {
+  let divElem = document.createElement('div');
+  let pElem = document.createElement('p');
+
+  pElem.textContent = 'Contact ' + i;
+
+  mainElem.appendChild(divElem);
+  divElem.appendChild(pElem);
+}
+
+// Allow submission of chat messages
+
+let form = document.querySelector('form');
+let input = document.querySelector('input');
+let messages = document.querySelector('.messages');
+messages.scrollTop = messages.scrollHeight;
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  let pElem = document.createElement('p');
+  pElem.textContent = 'Chris: ' + input.value;
+
+  messages.appendChild(pElem);
+  messages.scrollTop = messages.scrollHeight;
+
+  input.value = '';
+  input.focus();
+});
